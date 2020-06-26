af826c5231907712fa3740d280320e63c42484b6
Michael <michael.bosworth@bunchball.com>
2020-06-26 08:20:44 -0500

    fix start url

--- themes/attila/source/service-worker-template.js
+++ themes/attila/source/service-worker-template.js
@@ -1,7 +1,8 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute([
-  {url: '/', revision: 'GIT_HASH'}
+  {url: '/', revision: 'GIT_HASH'},
+  {url: '/index.html', revision: 'GIT_HASH'}
 ]);
 
 workbox.setConfig({
